ea2a46cf336d0f29ce6bd4a4d8ec8eb95548399f
steken <stekenno@gmail.com>
2019-10-22 17:19:45 +0100

    Update GUI for live recognition results

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import azure.cognitiveservices.speech as speechsdk #pylint: disable=import-error
 import time
+import wikipedia
+import click
+import sys
 
 class RecogEventHandler:
+    _last_output_recognizing = False
+
     def __init__(self, speech_recognizer):
         self._cancel_event_signal = speech_recognizer.canceled
         self._recognized_event_signal = speech_recognizer.recognized
@@ -20,24 +25,38 @@ class RecogEventHandler:
 
     def cancel_event_signal_handler(self, input, evt_args=0):
         print("[CANCEL_EVENT] " + str(input.cancellation_details))
-    
-    def recognized_event_signal_handler(self, input, evt_args=0):
-        print("[RECOGNIZED] UTTERANCE: " + str(input.result.text))
-
-    def recognizing_event_signal_handler(self, input, evt_args=0):
-        print("[RECOGNIZING] " + str(input.result.text))
+        self._last_output_recognizing = False
 
     def session_started_event_signal_handler(self, input, evt_args=0):
         print("[SESSION_START] " + str(input.session_id))
+        self._last_output_recognizing = False
 
     def session_stopped_event_signal_handler(self, input, evt_args=0):
-        print("[SESSION_STOP] " + str(input.session_id))
+        print('\n[SESSION_STOP] ' + str(input.session_id))
+        self._last_output_recognizing = False
+
+    # --- Speech Events
+
+    def recognized_event_signal_handler(self, input, evt_args=0):
+        print('\n[RECOGNIZED] \e[38;2;0;0;0m\e[48;2;255;255;255m' + str(input.result.text) + '\e[0m')
+        self._last_output_recognizing = False
+
+    def recognizing_event_signal_handler(self, input, evt_args=0):
+        if self._last_output_recognizing: 
+            # sys.stdout.flush()
+            # sys.stdout.write('\r[RECOGNIZING] ' + str(input.result.text), sep='', end='', flush=True)
+            print('\r[RECOGNIZING] ' + str(input.result.text), sep='', end='', flush=True)
+        else: 
+            print("[RECOGNIZING] " + str(input.result.text), sep='', end='', flush=True)
+            self._last_output_recognizing = True
 
     def speech_start_event_signal_handler(self, input, evt_args=0):
-        print("[SPEECH_START] " + str(input.offset))
+        print('\n[SPEECH_START] ' + str(input.offset))
+        self._last_output_recognizing = False
 
     def speech_stop_event_signal_handler(self, input, evt_args=0):
-        print("[SPEECH_STOP] " + str(input.offset))
+        print('\n[SPEECH_STOP] ' + str(input.offset))
+        self._last_output_recognizing = False
 
     @property
     def cancel_event_signal(self):
@@ -76,8 +95,10 @@ class RecogEventHandler:
 
 # TODO: 
 # [] Store keys in an ini file or something 
-# [] Implement smart recycling of the speech recognizer in between sentences after certain length of time elapsed ?
-# [] Implement eventlistener for some key to cancel while running with some basic terminal GUI 
+# [] a function for ranking the importance of a topic via the size of its page + references 
+# [] Implement eventlistener for some key to cancel while running with some basic terminal GUI via click
+#   [] Basic terminal GUI via Click (display topics)
+#   [x] Live updating on recognizing event
 # [] Implement some microphone processing to isolate speaking voices 
 # [] Implement NLP to extract dates and basic propositional sentences for lookup 
 # [] Implement a wikipedia scraper which hooks into NLP results 
@@ -91,8 +112,8 @@ recog_event_handler = RecogEventHandler(speech_recognizer)
 
 print("START SPEAKING 20s")
 test = speech_recognizer.start_continuous_recognition()
-time.sleep(20)
-speech_recognizer.stop_continuous_recognition()
+time.sleep(10)
+test.stop_continuous_recognition()
 print("END")
 
 
