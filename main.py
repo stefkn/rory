b4b9e202d49f6373f6865eb06d1ac8673489ae27
steken <stekenno@gmail.com>
2019-10-25 02:09:34 +0100

    add web ui

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import azure.cognitiveservices.speech as speechsdk #pylint: disable=import-error
 import time
-import wikipedia
-import click
+import wikipedia #pylint: disable=import-error
 import sys
 import re
-import pprint
+import pprint 
+import flask #pylint: disable=import-error
+import flask_socketio #pylint: disable=import-error
+import threading
+import os
+import json
+
+config = json.load(open('config.json'))
+
+# try:
+#     conf = open('keys.yaml')
+#     print(conf)
+# except Exception as e:
+#     print(e)
+# config.update_from_yaml_file(os.getcwd() + '/keys.yaml')
+
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = config['FLASK_SECRET_KEY']
+app.config['DEBUG'] = True
+socketio = flask_socketio.SocketIO(app)
 
 class RecogEventHandler:
     _speech_recognizer = None
@@ -35,6 +53,7 @@ class RecogEventHandler:
 
     def session_started_event_signal_handler(self, input, evt_args=0):
         print('\n[SESSION_START] ' + str(input.session_id))
+        socketio.emit('newevent', json.dumps({'text': 'Rory is now listening.'}), namespace='/siotest')
         self._last_output_recognizing = False
 
     def session_stopped_event_signal_handler(self, input, evt_args=0):
@@ -93,6 +112,17 @@ class RecogEventHandler:
                 print(str(match.group()))
             else:
                 self.print_summary(str(match.group()))
+                output = []
+                keyword_list = ["United States", "Great Britain", "United Kingdom", "Prime Minister", "President"]
+                for line in str(match.group()).split("\n"):
+                    for word in keyword_list:
+                        if word in line:
+                            output.append(line)
+                output_json = json.dumps({
+                    "text": inputyr,
+                    "longtext": output
+                })
+                socketio.emit('newevent', output_json, namespace='/siotest')
 
     def print_summary(self, page_content):
         for line in page_content.split("\n"):
@@ -134,30 +164,67 @@ class RecogEventHandler:
         """getter for SpeechStopEvent signaller""" 
         return self._speech_stop_event_signal
 
-# TODO: 
-# [] Store keys in an ini file or something 
-# [] a function for ranking the importance of a topic via the size of its page + references 
-# [] Implement eventlistener for some key to cancel while running with some basic terminal GUI via click
-#   [] Basic terminal GUI via Click (display topics)
-#   [x] Live updating on recognizing event
-# [] Implement some microphone processing to isolate speaking voices 
-# [] Implement NLP to extract dates and basic propositional sentences for lookup 
-# [] Implement a wikipedia scraper which hooks into NLP results 
-# [] Implement basic readout of scraped information 
+
+class SpeechRecogThread(threading.Thread):
+    def __init__(self, speech_recognizer):
+        super(SpeechRecogThread, self).__init__()
+        recog_event_handler = RecogEventHandler(speech_recognizer)
+
+    def startRecog(self):
+        print("START SPEAKING")
+        speech_recognizer.start_continuous_recognition()
+        time.sleep(30)
+        speech_recognizer.stop_continuous_recognition()
+        print("END")
+
+    def run(self):
+        self.startRecog()
 
 
-speech_key, service_region = "", ""
+class WebInterface():
+    global socketio
+
+    def __init__(self, speech_recognizer):
+        @app.route('/')
+        def index():
+            return flask.render_template('index.html')
+
+        @socketio.on('connect', namespace='/siotest')
+        def test_connection():
+            print('Client has connected.')
+            socketio.emit('newevent', json.dumps({'text': 'Connection Successful, starting up speech service.'}), namespace='/siotest')
+            thread = SpeechRecogThread(speech_recognizer)
+            thread.start()
+
+        @socketio.on('disconnect', namespace='/siotest')
+        def test_disconnection():
+            print('Client has disconnected.')
+            # Kill rory process here?
+
+        # if __name__ == '__main__':
+        socketio.run(app) 
+
+
+speech_key, service_region = config['AZURE_SPEECH_KEY'], config['AZURE_SERVICE_REGION']
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
-recog_event_handler = RecogEventHandler(speech_recognizer)
-
-print("START SPEAKING")
-speech_recognizer.start_continuous_recognition()
-time.sleep(30)
-speech_recognizer.stop_continuous_recognition()
-print("END")
+web_interface = WebInterface(speech_recognizer)
 
 
+# class SpeechRecogThread(threading.Thread):
+#     def __init__(self, speech_recognizer, web_interface):
+#         super(SpeechRecogThread, self).__init__()
+#         recog_event_handler = RecogEventHandler(speech_recognizer, web_interface)
+
+#     def startRecog(self):
+#         print("START SPEAKING")
+#         speech_recognizer.start_continuous_recognition()
+#         time.sleep(30)
+#         speech_recognizer.stop_continuous_recognition()
+#         print("END")
+
+#     def run(self):
+#         self.startRecog()
 
 
 # if process == 'not finished':
